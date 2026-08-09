66f34cf247d582d9049a1f4b8bcb77457b302c60
Anmol <76591672+AnmolMogalayi@users.noreply.github.com>
2026-08-09 22:33:59 +0530

    git

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$ei6=a3tm50so)v28&5%z^dr0!1rj(f-#65jk+blb-h8ju*ld#'
+SECRET_KEY = os.environ.get('SECRET_KEY')    
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
